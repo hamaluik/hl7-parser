45f9f68ea4627d894a7329cc4f71794871c795e9
Kenton Hamaluik <kenton@hamaluik.ca>
2023-03-14 19:53:29 -0600

    compare performance against rust-hl7

--- benches/library_compare.rs
+++ benches/library_compare.rs
@@ -1,8 +1,8 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use hl7_parser::*;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("parse adt^a01 hl7-parser", |b| {
-        use hl7_parser::*;
+    c.bench_function("parse adt^a01", |b| {
         let message = include_str!("../test_assets/sample_adt_a01.hl7")
             .replace("\r\n", "\r")
             .replace('\n', "\r");
@@ -11,14 +11,21 @@ fn criterion_benchmark(c: &mut Criterion) {
         })
     });
 
-    c.bench_function("parse adt^a01 rust-hl7", |b| {
-        use rusthl7::*;
+    c.bench_function("decode encoded sequences", |b| {
+        let separators = Separators::default();
+        b.iter(|| separators.decode(black_box(r#"\F\\R\\S\\T\\E\"#)))
+    });
+
+    c.bench_function("locate cursor", |b| {
         let message = include_str!("../test_assets/sample_adt_a01.hl7")
             .replace("\r\n", "\r")
             .replace('\n', "\r");
-        b.iter(|| {
-            message::Message::try_from(black_box(message.as_str())).expect("can parse message");
-        })
+        let message = Message::parse(message.as_str()).expect("can parse message");
+        b.iter(|| message.locate_cursor(black_box(0x458)))
+    });
+
+    c.bench_function("parse timestamp", |b| {
+        b.iter(|| parse_time(black_box("20230312195905.1234-0700")).expect("can parse timestamp"))
     });
 }
 
