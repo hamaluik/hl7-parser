150ad79e2622e13f781b2baa96dcbde8cbecb86e
Kenton Hamaluik <kenton@hamaluik.ca>
2024-09-21 23:10:03 -0600

    todo list

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,15 @@ pub use message::*;
 /// Functions to parse various parts of an HL7 message. Probably not useful to you
 /// (use the `Message::parse` method instead).
 pub mod parser;
+
+// TODO list:
+//
+// - Add lenient parsing for segment separators (e.g. allow \n or \r\n as well as \r)
+// - Add cursor location
+// - Add query functions to get fields, components, etc. by name
+// - Add ability to convert parsed messages into a mutable form that can be modified and then
+// serialized back into a hl7 message
+// - Add serde support
+// - More tests
+// - More documentation
+// - More examples
